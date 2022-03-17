0947e9b26c5dbbac08989176f477d82d981a6d44
tho2622003 <tho2622003@gmail.com>
2022-03-17 17:48:10 +0700

    Update testwindow.cpp

--- pikachu/testwindow.cpp
+++ pikachu/testwindow.cpp
@@ -8,11 +8,38 @@ int  main(int argc, char** argv) {
 
     bool running = true;
 
-    while(running) {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+    SDL_Event quit;
 
+    while(running) {
+        SDL_SetRenderDrawColor(renderer, 120, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        //begin of insert dumb stuffs here
+
+        //draw a line
+        SDL_SetRenderDrawColor(renderer, 200, 100, 0, 255);
+        SDL_RenderDrawLine(renderer, 0, 0, 50, 50);
+
+        //define and draw a rectangle
+        SDL_Rect location{100, 100, 100, 100};
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &location);
+
+        //fill the previous rectangle
+        SDL_SetRenderDrawColor(renderer, 25, 255, 50, 255);
+        SDL_RenderFillRect(renderer, &location);
+
+        //end of insert dumb stuffs here
         SDL_RenderPresent(renderer);
+
+        while(SDL_PollEvent(&quit)){
+            switch(quit.type){
+            case SDL_QUIT:
+                running = false;
+                break;
+            default:
+                break;
+            }
+        }
     }
 
     SDL_Quit();
