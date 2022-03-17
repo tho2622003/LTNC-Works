e8e3b1ec50abf5475a43dd25e7926c395ec21b38
tho2622003 <tho2622003@gmail.com>
2022-03-17 20:57:38 +0700

    Update testwindow.cpp

--- pikachu/testwindow.cpp
+++ pikachu/testwindow.cpp
@@ -3,15 +3,20 @@
 int  main(int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO);
 
+    //create a window
     SDL_Window* window = SDL_CreateWindow("testpikachu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+
+    //create a renderer
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+    //boolean to check if program is running correctly
     bool running = true;
 
+    //make an event to handle quitting program
     SDL_Event quit;
 
     while(running) {
-        SDL_SetRenderDrawColor(renderer, 120, 0, 0, 255);
+        SDL_SetRenderDrawColor(renderer, 64, 204, 208, 255);
         SDL_RenderClear(renderer);
         //begin of insert dumb stuffs here
 
@@ -19,19 +24,21 @@ int  main(int argc, char** argv) {
         SDL_SetRenderDrawColor(renderer, 200, 100, 0, 255);
         SDL_RenderDrawLine(renderer, 0, 0, 50, 50);
 
-        //define and draw a rectangle
+        //define a rectangle
         SDL_Rect location{100, 100, 100, 100};
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderDrawRect(renderer, &location);
 
-        //fill the previous rectangle
+        /*draw a stroke-only (no fill) rectangle
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawRect(renderer, &location);*/
+
+        //draw a color filled rectangle
         SDL_SetRenderDrawColor(renderer, 25, 255, 50, 255);
         SDL_RenderFillRect(renderer, &location);
 
         //end of insert dumb stuffs here
         SDL_RenderPresent(renderer);
 
-        //quit event while loop
+        //a while loop for the quit event
         while(SDL_PollEvent(&quit)){
             switch(quit.type){
             //click x to quit window (SDL_QUIT != SDL_Quit())
