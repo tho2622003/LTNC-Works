5fdcaf051bb0d9f235e4425d1b3da12f23f19a14
tho2622003 <tho2622003@gmail.com>
2022-03-18 09:57:02 +0700

    the damn code finally runs

--- pikachu/testmouse.cpp
+++ pikachu/testmouse.cpp
@@ -4,16 +4,21 @@
 int main (int argc, char** argv) {
     SDL_Init(SDL_INIT_VIDEO);
     bool running = true;
-    SDL_Window* window = SDL_CreateWindow("testing cursor", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    //make a window
+    SDL_Window* window = SDL_CreateWindow("pikachu pika pika pika", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+    //make a renderer, the same old stuff
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //make a surface to be transferred onto
     SDL_Surface* surface = SDL_GetWindowSurface(window);
+    //make a bitmap surface to load image
     SDL_Surface* bitmap = NULL;
     while(running) {
-
+        //load the damn bitmap then boil... blit the hell out of it
         bitmap = SDL_LoadBMP("../pikachu/smug.bmp");
         SDL_BlitSurface(bitmap, NULL, surface, NULL);
         SDL_UpdateWindowSurface(window);
 
+        //event for quiting and tracking mouse coordinates
         SDL_Event event;
         while(SDL_PollEvent(&event)){
             switch(event.type){
@@ -31,6 +36,7 @@ int main (int argc, char** argv) {
             }
         }
     }
+    //typical exit procedures
     SDL_FreeSurface(bitmap);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
